f321a788d2203552fa33baf782d3af846d9acece
MrKanYT <mrkan2017@gmail.com>
2022-04-10 15:35:01 +0400

    mqtt manager обновлен

--- mysite/MQTTManager.py
+++ mysite/MQTTManager.py
@@ -1,24 +1,22 @@
-import time
-
 import paho.mqtt.client as MQTT
 
+'''
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    print("message received ", str(message.payload.decode("utf-8")))
+    print("message topic=", message.topic)
+    print("message qos=", message.qos)
+    print("message retain flag=", message.retain)
+'''
 
 class MQTTManager:
 
     client = None
+    topicHandlers = {}
     host = ""
     port = 0
     user = ""
     password = ""
     prefix = "WEB/"
-
-    def on_message_test(self, message, **kwargs):
-        print(message)
 
     def connect(self):
         self.client = MQTT.Client("WEB1")
@@ -29,12 +27,12 @@ class MQTTManager:
         self.client.loop_start()
         for t in self.topicHandlers.keys():
             self.client.subscribe(self.prefix + t)
-            print(f'MQTT: Subscribed to {self.prefix + t}')
+        print(f'MQTT: Subscribed to {", ".join([self.prefix + n for n in self.topicHandlers.keys()])}')
 
     def on_message(self, userdata, message):
+        print(f'MQTT: [{message.topic.replace(self.prefix, "")}] - {str(message.payload.decode("utf-8"))}')
         if message.topic.replace(self.prefix, '') in self.topicHandlers.keys():
-            self.topicHandlers[message.topic.replace(self.prefix, "")](self, str(message.payload.decode("utf-8")))
-            print(f'MQTT: [{message.topic.replace(self.prefix, "")}] - {str(message.payload.decode("utf-8"))}')
+            self.topicHandlers[message.topic.replace(self.prefix, "")](str(message.payload.decode("utf-8")))
 
     def __init__(self, host, port, user, password):
         self.host = str(host)
@@ -42,17 +40,16 @@ class MQTTManager:
         self.user = str(user)
         self.password = str(password)
 
-    topicHandlers = {
-        "test": on_message_test
-    }
+def on_message_test(message):
+    print(message)
 
 if __name__ == "__main__":
     cl = MQTTManager('185.134.36.37', '18883', 'WEB', '456456')
+    cl.topicHandlers['test'] = on_message_test
     cl.connect()
 
     while True:
         pass
-
 
 """
 Prg: 
